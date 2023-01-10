def1dce46ad3f237c159f0a4b290ed549b01cebb
hoagtu <trantu2000hp@gmail.com>
2023-01-10 19:42:28 +0700

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double height;
+            Console.WriteLine("Enter the dimensions of the cylinder");
+            Console.Write("Radius: ");
+            var radiusInput = Console.ReadLine().ToString();
+            Console.Write("Height: ");
+            var heightInput = Console.ReadLine().ToString();
+            try
+            {
+                radius = double.Parse(radiusInput);
+                height = double.Parse(heightInput);
+                var cylinder = new Cylinder(radius, height);
+                cylinder.Process();
+                cylinder.Result(out double baseArea, out double lateralArea, out double totalArea, out double volume);
+                Console.WriteLine("Cylinder Characteristics");
+                Console.WriteLine($"Raidus: {radius}, Height: {height}");
+                Console.WriteLine($"Base: {baseArea}, ¦ Lateral: {lateralArea} ¦ Total: {totalArea} ¦ Volume: {volume}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Invalid dimensions: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Exercise2/Tiger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    internal class Tiger : Animal
+    {
+        public Tiger(double weight)
+        {
+            base.SetMe(weight, "Tiger");
+        }
+    }
+}
